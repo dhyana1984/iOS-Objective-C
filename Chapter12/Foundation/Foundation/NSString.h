9bef451a36f98539ca65805c0bc8c00d777bfb07
dhyana1984 <xiongyi_1984@foxmail.com>
2019-11-09 00:53:23 +0800

    Fundation NSString

---
+++ Chapter12/Foundation/Foundation/NSString.h
@@ -0,0 +1,17 @@
+//
+//  NSString.h
+//  Foundation
+//
+//  Created by Chris Xiong on 2019/11/9.
+//  Copyright © 2019 org.chris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Chapter12/Foundation/Foundation/NSString.m
@@ -0,0 +1,13 @@
+//
+//  NSString.m
+//  Foundation
+//
+//  Created by Chris Xiong on 2019/11/9.
+//  Copyright © 2019 org.chris. All rights reserved.
+//
+
+#import "NSString.h"
+
+@implementation NSString
+
+@end
